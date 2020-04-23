0be30b4613af70bdd1cd4abb407d293c4ebc3b68
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-04-23 21:54:37 +0100

    extracted magic numbers from PDFFileDownloader

--- src/main/kotlin/org/ionproject/integration/file/implementation/PDFFileDownloader.kt
+++ src/main/kotlin/org/ionproject/integration/file/implementation/PDFFileDownloader.kt
@@ -2,13 +2,15 @@ package org.ionproject.integration.file.implementation
 
 class PDFFileDownloader : AbstractFileDownloader("PDF") {
     private val PDF_HEADER = "%PDF-1."
-    private val MIN_PDF_VERSION = '0'
-    private val MAX_PDF_VERSION = '7'
+    private val MINIMUM_PDF_VERSION = '0'
+    private val MAXIMUM_PDF_VERSION = '7'
+    private val HEADER_MIN_VERSION_POSITION = 7
+    private val HEADER_RANGE = 0..6
 
     override fun checkFormat(bytes: ByteArray): Boolean {
-        val version: Char = bytes[7].toChar()
-        val headerBytes: ByteArray = bytes.slice(0..6).toByteArray()
+        val minorVersion: Char = bytes[HEADER_MIN_VERSION_POSITION].toChar()
+        val headerBytes: ByteArray = bytes.slice(HEADER_RANGE).toByteArray()
         val header = String(headerBytes, Charsets.UTF_8)
-        return header == PDF_HEADER && version in MIN_PDF_VERSION..MAX_PDF_VERSION
+        return header == PDF_HEADER && minorVersion in MINIMUM_PDF_VERSION..MAXIMUM_PDF_VERSION
     }
 }
