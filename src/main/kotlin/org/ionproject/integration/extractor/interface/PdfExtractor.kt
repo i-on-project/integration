e1824c8f980cbee2b2ee4c521ec223d221de21f0
Ts1n <miguelbarbosateixeira@gmail.com>
2020-04-26 21:12:28 +0100

    Add PdfExtractor interface

---
+++ src/main/kotlin/org/ionproject/integration/extractor/interface/PdfExtractor.kt
@@ -0,0 +1,13 @@
+package org.ionproject.integration.extractor.`interface`
+
+import org.ionproject.integration.utils.Try
+
+interface PdfExtractor {
+    /**
+     * Extract data from pdf file locate at [pdfPath]
+     * @return Try<MutableList<String>>
+     *     Success - String list contains extracted data
+     *     Failure - PdfExtractorException
+     */
+    suspend fun extract(pdfPath: String): Try<MutableList<String>>
+}
