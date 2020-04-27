60ec77271c79b3410090553d657d7db9afd7af47
Ts1n <miguelbarbosateixeira@gmail.com>
2020-04-27 02:24:45 +0100

    Add TabulaPdfExtractor

---
+++ src/main/kotlin/org/ionproject/integration/extractor/implementation/TabulaPdfExtractor.kt
@@ -0,0 +1,42 @@
+package org.ionproject.integration.extractor.implementation
+
+import java.io.File
+import org.apache.commons.cli.DefaultParser
+import org.ionproject.integration.extractor.`interface`.PdfExtractor
+import org.ionproject.integration.extractor.exception.PdfExtractorException
+import org.ionproject.integration.utils.Try
+import technology.tabula.CommandLineApp
+
+class TabulaPdfExtractor : PdfExtractor {
+    /**
+     * Extract table data from pdf file locate at [pdfPath]
+     * @return Pair<Boolean, String>
+     *     Boolean - Indicates if extracting was successful
+     *     String list - Contains all extracted data in json format
+     */
+    override suspend fun extract(pdfPath: String): Try<MutableList<String>> {
+        if (pdfPath.isEmpty()) return Try.ofError(PdfExtractorException("Empty path"))
+
+        val pdfFile = File(pdfPath)
+
+        if (!pdfFile.exists()) return Try.ofError(PdfExtractorException("File doesn't exist"))
+
+        // Arguments to pass to tabula
+        // -g = Guess the portion of the page to analyze per page
+        // -l = Force PDF to be extracted using lattice-mode extraction
+        // -p = Page range
+        // -f = Output format JSON
+        val args = arrayOf(pdfFile.absolutePath, "-g", "-l", "-p", "all", "-f", "JSON")
+        val parser = DefaultParser()
+        val cmd = parser.parse(CommandLineApp.buildOptions(), args)
+
+        // CommandLineApp(data, cmd).extractTables(cmd)
+        val data = StringBuilder()
+        var result = Try
+            .of { CommandLineApp(data, cmd).extractTables(cmd) }
+
+        return if (result is Try.Error) {
+            Try.ofError(PdfExtractorException("Tabula cannot process file"))
+        } else Try.of(mutableListOf(data.toString()))
+    }
+}
