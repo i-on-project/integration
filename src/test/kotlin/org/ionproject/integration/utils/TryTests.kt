68c529e75d8a2edd8e3451ae876013ab54a4b89e
Miguel Teixeira <miguelbarbosateixeira@gmail.com>
2020-05-07 14:19:59 +0100

    Tests refactoring

--- src/test/kotlin/org/ionproject/integration/utils/TryTests.kt
+++ src/test/kotlin/org/ionproject/integration/utils/TryTests.kt
@@ -2,28 +2,124 @@ package org.ionproject.integration.utils
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 class TryTests {
 
-    fun oper(a: Int, b: Int) =
+    private fun oper(a: Int, b: Int) =
         Try.of { a / b }
             .map { it + 1 }
             .map { it * 2 }
 
     @Test
-    fun test() {
-        assertEquals("/ by zero", oper(1, 0).match({ it.toString() }, { it.message }))
-        assertEquals("6", oper(2, 1).match({ it.toString() }, { it.message }))
-        assertEquals("error", oper(1, 0).map { it.toString() }.orElse("error"))
-        assertEquals("6", oper(2, 1).map { it.toString() }.orElse("error"))
+    fun whenNonZeroOperands_thenReturnsArithmeticResult() {
+        // Act
+        val result = oper(2, 1)
+            .match({ it.toString() }, { it.message })
 
-        assertEquals("CompositeException",
-            Try.map(oper(1, 0), oper(1, 0)) { a, b -> (a + b).toString() }.orElse { it.javaClass.simpleName })
-        assertEquals("ArithmeticException",
-            Try.map(oper(2, 1), oper(1, 0)) { a, b -> (a + b).toString() }.orElse { it.javaClass.simpleName })
-        assertEquals("ArithmeticException",
-            Try.map(oper(1, 0), oper(2, 1)) { a, b -> (a + b).toString() }.orElse { it.javaClass.simpleName })
-        assertEquals("12",
-            Try.map(oper(2, 1), oper(2, 1)) { a, b -> (a + b).toString() }.orElse { it.javaClass.simpleName })
+        // Assert
+        assertEquals("6", result)
+    }
+
+    @Test
+    fun whenNonZeroOperandsAndDefaultError_thenReturnsArithmeticResult() {
+        // Act
+        val result = oper(2, 1)
+            .map { it.toString() }
+            .orElse("error")
+
+        // Assert
+        assertEquals("6", result)
+    }
+
+    @Test
+    fun whenNonZeroOperandsAndDefaultThrow_thenReturnsArithmeticResult() {
+        // Act
+        val result = oper(2, 1)
+            .map { it.toString() }
+
+        // Assert
+        assertDoesNotThrow { result.orThrow() }
+        assertEquals("6", result.get())
+    }
+
+    @Test
+    fun whenDivideByZero_thenReturnsErrorWithArithmeticExceptionMessage() {
+        // Act
+        val result = oper(1, 0)
+            .match({ it.toString() }, { it.message })
+
+        // Assert
+        assertEquals("/ by zero", result)
+    }
+
+    @Test
+    fun whenDivideByZeroAndDefaultError_thenReturnsDefaultError() {
+        // Act
+        val result = oper(1, 0)
+            .map { it.toString() }
+            .orElse("error")
+
+        // Assert
+        assertEquals("error", result)
+    }
+
+    @Test
+    fun whenDivideByZeroAndDefaultThrow_thenThrowsArithmeticException() {
+        // Act
+        val result = oper(1, 0)
+            .map { it.toString() }
+
+        // Assert
+        assertThrows<ArithmeticException> { result.orThrow() }
+    }
+
+    @Test
+    fun whenOfValue_thenValueInstance() {
+        // Act
+        val result = Try.ofValue(1)
+
+        // Assert
+        assertEquals("1", result.get())
+    }
+
+    @Test
+    fun whenOfError_thenErrorInstance() {
+        // Act
+        val result = Try.ofError(Exception())
+
+        // Assert
+        assertThrows<Exception> { result.get() }
+    }
+
+    @Test
+    fun whenMultipleOperWithNonZeroOperands_thenReturnsArithmeticResult() {
+        // Act
+        val result = Try.map(oper(2, 1), oper(2, 1)) { a, b -> (a + b).toString() }
+            .orElse { it.javaClass.simpleName }
+
+        // Assert
+        assertEquals("12", result)
+    }
+
+    @Test
+    fun whenMultipleOperOneDivideByZero_thenReturnsErrorWithArithmeticExceptionMessage() {
+        // Act
+        val result = Try.map(oper(2, 1), oper(1, 0)) { a, b -> (a + b).toString() }
+            .orElse { it.javaClass.simpleName }
+
+        // Assert
+        assertEquals("ArithmeticException", result)
+    }
+
+    @Test
+    fun whenMultipleOperAllDivideByZero_thenReturnsErrorWithCompositeExceptionMessage() {
+        // Act
+        val result = Try.map(oper(1, 0), oper(1, 0)) { a, b -> (a + b).toString() }
+            .orElse { it.javaClass.simpleName }
+
+        // Assert
+        assertEquals("CompositeException", result)
     }
 }
