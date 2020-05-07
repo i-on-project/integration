0364a0853749cdc6b8fd56113419161d1f909aac
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-05-07 18:21:21 +0100

    added response class

---
+++ src/main/kotlin/org/ionproject/integration/model/internal/Response.kt
@@ -0,0 +1,21 @@
+package org.ionproject.integration.model.internal
+
+data class Response(val statusCode : Int, val body : ByteArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Response
+
+        if (statusCode != other.statusCode) return false
+        if (!body.contentEquals(other.body)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = statusCode
+        result = 31 * result + body.contentHashCode()
+        return result
+    }
+}
