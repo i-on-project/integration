093069d4a4497771d9e67acd660f3554be832785
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-05-07 18:21:03 +0100

    added fastpath to checkFormat in PDFFileDownloader

--- src/main/kotlin/org/ionproject/integration/file/implementation/PDFFileDownloader.kt
+++ src/main/kotlin/org/ionproject/integration/file/implementation/PDFFileDownloader.kt
@@ -8,8 +8,11 @@ class PDFFileDownloader : AbstractFileDownloader() {
     private val MAXIMUM_PDF_VERSION = '7'
     private val HEADER_MIN_VERSION_POSITION = 7
     private val HEADER_RANGE = 0..6
+    private val HEADER_LENGTH = 7
 
     override fun checkFormat(bytes: ByteArray) {
+        if(bytes.size < HEADER_LENGTH)
+            throw InvalidFormatException("Downloaded content was not in the PDF format.")
         val minorVersion: Char = bytes[HEADER_MIN_VERSION_POSITION].toChar()
         val headerBytes: ByteArray = bytes.slice(HEADER_RANGE).toByteArray()
         val header = String(headerBytes, Charsets.UTF_8)
