fe9d4af167d63273eebe0dd7ee3993b9210c72ba
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-05-07 18:20:16 +0100

    updated AbstractFileDownloader

--- src/main/kotlin/org/ionproject/integration/file/implementation/AbstractFileDownloader.kt
+++ src/main/kotlin/org/ionproject/integration/file/implementation/AbstractFileDownloader.kt
@@ -6,7 +6,12 @@ import java.net.URL
 import java.nio.file.Path
 import org.ionproject.integration.file.`interface`.FileDownloader
 import org.ionproject.integration.file.exception.ServerErrorException
+import org.ionproject.integration.model.internal.Response
 import org.ionproject.integration.utils.Try
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 abstract class AbstractFileDownloader() :
     FileDownloader {
@@ -16,30 +21,31 @@ abstract class AbstractFileDownloader() :
             return Try.ofError(IllegalArgumentException("Parameters url and localDestination need not be empty"))
         }
 
-        val url = URL(urlStr)
+        val client = Try.of(HttpClient.newHttpClient())
 
-        val httpConn = Try.of(url.openConnection() as HttpURLConnection)
+        val request = Try.of(
+            HttpRequest.newBuilder()
+            .uri(URI.create(urlStr))
+            .build())
 
-        val responseCode = httpConn
-            .map { conn -> conn.responseCode }
-            .map { code -> throwIfServerError(code) }
+        val response = Try.map(client, request)
+        {c, r -> c.send(r, HttpResponse.BodyHandlers.ofByteArray())}
+            .map { resp -> Response(resp.statusCode(),resp.body()) }
+            .flatMap { resp -> validateResponseCode(resp) }
 
-        val bytes = Try.map(responseCode, httpConn) { _, conn -> conn.inputStream.readBytes() }
+        val file = response.map { r -> checkFormat(r.body) }
+            .map { File(localDestination) }
 
-        val isValidFormat = bytes.map { byteArray -> checkFormat(byteArray) }
-
-        val file = isValidFormat.map { File(localDestination) }
-
-        Try.map(file, bytes) { f: File, b: ByteArray -> f.writeBytes(b) }
+        Try.map(file, response) { f, r -> f.writeBytes(r.body)}
 
         return file.map { f -> f.toPath() }
     }
 
-    private fun throwIfServerError(code: Int): Int {
-        return when (code) {
+    private fun validateResponseCode(response : Response): Try<Response> {
+        return when (response.statusCode) {
             in HttpURLConnection.HTTP_INTERNAL_ERROR..HttpURLConnection.HTTP_VERSION ->
-                throw ServerErrorException("Server responded with error code $code")
-            else -> code
+                return Try.ofError(ServerErrorException("Server responded with error code ${response.statusCode}"))
+            else -> Try.of(response)
         }
     }
 
