9d9560d4ed39eb8f81ab8f4ede2160bb33c0aed3
Ts1n <miguelbarbosateixeira@gmail.com>
2020-05-28 22:04:29 +0100

    sourceCompability upgraded to java11 to match docker base image

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "org.ionproject"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 val imageId = "docker.pkg.github.com/i-on-project/integration/i-on-integration"
 val tempDockerTag: String = "i-on-integration-image"
