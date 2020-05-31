60ad4e8f865a37ffc303ea4ed581597b0efbbbac
Ts1n <miguelbarbosateixeira@gmail.com>
2020-05-31 17:51:01 +0100

    Incorrect Transient property

--- src/main/kotlin/org/ionproject/integration/model/internal/timetable/School.kt
+++ src/main/kotlin/org/ionproject/integration/model/internal/timetable/School.kt
@@ -1,6 +1,6 @@
 package org.ionproject.integration.model.internal.timetable
 
 data class School(
-    @Transient var name: String = "",
-    var acr: String = ""
+    var name: String = "",
+    @Transient var acr: String = ""
 )
