c004e7d3013957c68401a7056083e9c641a9ac3a
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-06-10 11:31:30 +0100

    logs json on upload tasklet

--- src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/FacultyTasklet.kt
+++ src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/FacultyTasklet.kt
@@ -5,6 +5,7 @@ import org.ionproject.integration.job.ISELTimetable
 import org.ionproject.integration.utils.JsonUtils
 import org.ionproject.integration.utils.Try
 import org.ionproject.integration.utils.orThrow
+import org.slf4j.LoggerFactory
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
@@ -12,12 +13,13 @@ import org.springframework.batch.repeat.RepeatStatus
 
 class FacultyTasklet(val properties: ISELTimetableProperties, val state: ISELTimetable.State) :
     Tasklet {
+    val log = LoggerFactory.getLogger(FacultyTasklet::class.java)
     override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
 
         val json = Try.ofValue(state.timetableTeachers.teachers.toTypedArray())
             .flatMap { t -> JsonUtils.toJson(t) }
 
-        println(json.orThrow())
+        log.info(json.orThrow())
         return RepeatStatus.FINISHED
     }
 }
--- src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/TimetableTasklet.kt
+++ src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/TimetableTasklet.kt
@@ -5,6 +5,7 @@ import org.ionproject.integration.job.ISELTimetable
 import org.ionproject.integration.utils.JsonUtils
 import org.ionproject.integration.utils.Try
 import org.ionproject.integration.utils.orThrow
+import org.slf4j.LoggerFactory
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
@@ -12,14 +13,14 @@ import org.springframework.batch.repeat.RepeatStatus
 
 class TimetableTasklet(val props: ISELTimetableProperties, val state: ISELTimetable.State) :
     Tasklet {
+    val log = LoggerFactory.getLogger(TimetableTasklet::class.java)
     override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
         println("sent timetable to core - ${state.timetableTeachers.timetable}")
 
         val json = Try.ofValue(state.timetableTeachers.timetable.toTypedArray())
             .flatMap { t -> JsonUtils.toJson(t) }
 
-        println(json.orThrow())
-
+        log.info(json.orThrow())
         return RepeatStatus.FINISHED
     }
 }
