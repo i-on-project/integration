3646fb3bf04c43dab72bb664387521ed7b7eff2d
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-06-17 19:50:01 +0100

    corrected comment inside runjob method

--- src/main/kotlin/org/ionproject/integration/IOnIntegrationApplication.kt
+++ src/main/kotlin/org/ionproject/integration/IOnIntegrationApplication.kt
@@ -53,7 +53,8 @@ class IOnIntegrationApplication
                     // We want to detect executions of the job
                     // for the same course without peeking into the pdf.
                     // One way is to concat all the jobParameters
-                    // with job name. It does not detect all,
+                    // with job name and calculate the hash of the resulting string.
+                    // It does not detect all,
                     // but a reasonable amount of times.
                     val jobHash = jobName.plus(it.entries.joinToString()).hashCode()
                     jobParametersBuilder.addString("jobId", jobHash.toString())
