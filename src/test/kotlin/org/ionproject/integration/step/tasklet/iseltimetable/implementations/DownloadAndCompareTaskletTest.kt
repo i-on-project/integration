1c085f77497f8b52d48c4887a9952f1ff13b7e10
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-06-21 12:38:41 +0100

    added testpropertysource to downloadandcomparetasklet error test

--- src/test/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/DownloadAndCompareTaskletTest.kt
+++ src/test/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/DownloadAndCompareTaskletTest.kt
@@ -21,6 +21,7 @@ import org.springframework.batch.test.context.SpringBatchTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
@@ -218,6 +219,13 @@ internal class DownloadAndCompareTaskletUrlNotPdfTest {
         ISELTimetable::class,
         DownloadAndCompareTasklet::class,
         IOnIntegrationApplication::class
+    ]
+)
+@TestPropertySource(
+    properties = [
+        "ion.core-base-url = test",
+        "ion.core-token = test",
+        "ion.core-request-timeout-seconds = 1"
     ]
 )
 @SpringBatchTest
