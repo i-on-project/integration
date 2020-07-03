0ccde04b1efe051b6de9f6ebf56662ab9975bd07
Miguel Teixeira <miguelbarbosateixeira@gmail.com>
2020-07-03 15:51:17 +0100

    Added logging and alerts to UploadTasklet

--- src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/UploadTasklet.kt
+++ src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/UploadTasklet.kt
@@ -1,5 +1,6 @@
 package org.ionproject.integration.step.tasklet.iseltimetable.implementations
 
+import org.ionproject.integration.alert.implementations.EmailAlertService
 import org.ionproject.integration.config.AppProperties
 import org.ionproject.integration.job.ISELTimetable
 import org.ionproject.integration.model.internal.core.CoreResult
@@ -12,11 +13,17 @@ import org.springframework.batch.core.scope.context.ChunkContext
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Scope
+import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.stereotype.Component
 
 @Component
 @Scope(value = "prototype")
-class UploadTasklet(private val coreService: CoreService, private val appProperties: AppProperties, private val state: ISELTimetable.State) : Tasklet {
+class UploadTasklet(
+    private val coreService: CoreService,
+    private val appProperties: AppProperties,
+    private val state: ISELTimetable.State,
+    private val sender: JavaMailSenderImpl
+) : Tasklet {
 
     private val contextKey = "CoreRetries"
     private val log = LoggerFactory.getLogger(UploadTasklet::class.java)
@@ -38,11 +45,14 @@ class UploadTasklet(private val coreService: CoreService, private val appPropert
             retries = jobContext.getInt(contextKey)
         }
 
-        when (val coreResult = uploadToCore()) {
+        val coreResult = uploadToCore()
+        when (coreResult) {
             CoreResult.TRY_AGAIN -> {
                 retries--
             }
             else -> {
+                log.error("I-On Core replied with $coreResult")
+                sendEmail(coreResult, chunkContext)
                 return RepeatStatus.FINISHED
             }
         }
@@ -50,6 +60,8 @@ class UploadTasklet(private val coreService: CoreService, private val appPropert
         jobContext.putInt(contextKey, retries)
 
         return if (retries == 0) {
+            log.warn("I-On Core unreachable")
+            sendEmail(coreResult, chunkContext)
             RepeatStatus.FINISHED
         } else {
             RepeatStatus.CONTINUABLE
@@ -59,5 +71,22 @@ class UploadTasklet(private val coreService: CoreService, private val appPropert
     private fun uploadToCore() = when (uploadType) {
         UploadType.TIMETABLE -> coreService.pushTimetable(state.timetableTeachers.timetable).orThrow()
         UploadType.TEACHERS -> coreService.pushCourseTeacher(state.timetableTeachers.teachers).orThrow()
+    }
+
+    private fun sendEmail(coreResult: CoreResult, context: ChunkContext) {
+        val alertRecipient = context.stepContext.jobParameters["alertRecipient"] as String
+        val pdfRemoteLocation = context.stepContext.jobParameters["pdfRemoteLocation"] as String
+
+        val asset = pdfRemoteLocation.substring(pdfRemoteLocation.lastIndexOf('/') + 1, pdfRemoteLocation.length)
+        val alertService = EmailAlertService("ISEL Timetable Batch Job", alertRecipient, asset, sender)
+
+        val message = when (coreResult) {
+            CoreResult.TRY_AGAIN -> "I-On Core was unreachable with multiple retries"
+            CoreResult.EXPIRED_TOKEN -> "I-On Core authentication token expired or is no longer valid"
+            CoreResult.INVALID_JSON -> "Payload sent to I-On Core no longer valid"
+            else -> "I-On Core unknown error"
+        }
+
+        alertService.sendFailureEmail(message)
     }
 }
