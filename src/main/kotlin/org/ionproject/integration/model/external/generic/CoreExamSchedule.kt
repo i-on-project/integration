5ea69ffdca2edd412e6d942f5a8d6fa2251245d3
Samuel Costa <43552@alunos.isel.ipl.pt>
2020-07-08 19:57:02 +0100

    formatted

--- src/main/kotlin/org/ionproject/integration/model/external/generic/CoreExamSchedule.kt
+++ src/main/kotlin/org/ionproject/integration/model/external/generic/CoreExamSchedule.kt
@@ -2,4 +2,4 @@ package org.ionproject.integration.model.external.generic
 
 import org.ionproject.integration.model.internal.generic.ICoreModel
 
-class CoreExamSchedule(): ICoreModel
+class CoreExamSchedule() : ICoreModel
