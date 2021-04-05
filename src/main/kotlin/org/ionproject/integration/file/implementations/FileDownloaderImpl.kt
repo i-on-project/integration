680d5ffe81ce5254be0effbc30c1dd254e688f62
Grimord <grimord@hotmail.com>
2021-04-05 23:21:59 +0100

    Update FileDownloaderImpl.kt

--- src/main/kotlin/org/ionproject/integration/file/implementations/FileDownloaderImpl.kt
+++ src/main/kotlin/org/ionproject/integration/file/implementations/FileDownloaderImpl.kt
@@ -38,7 +38,7 @@ class FileDownloaderImpl(private val checker: IBytesFormatChecker) :
         val file = response.map { r -> checker.checkFormat(r.body, jobType) }
             .map { localDestination.toFile() }
 
-        println("File path: ${localDestination.fileName}")
+        println("File path: $localDestination")
 
         return Try.map(file, response) { f, r -> f.writeBytes(r.body); f.toPath() }
     }
