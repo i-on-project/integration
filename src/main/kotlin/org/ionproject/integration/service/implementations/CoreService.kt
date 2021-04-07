abae5abcf52ab785a5fbe31d8fc05fbc09526ce6
Ricardo Canto <rjcanto@gmail.com>
2021-04-08 00:08:20 +0100

    Removed Write API calls in CoreService.kt
    Created StringLiterals.kt but considering if literals should be added to AppProperties.kt instead.

--- src/main/kotlin/org/ionproject/integration/service/implementations/CoreService.kt
+++ src/main/kotlin/org/ionproject/integration/service/implementations/CoreService.kt
@@ -1,11 +1,6 @@
 package org.ionproject.integration.service.implementations
 
 import java.io.File
-import java.net.HttpURLConnection
-import java.net.URI
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-import java.time.Duration
 import org.ionproject.integration.config.AppProperties
 import org.ionproject.integration.model.external.generic.CoreExamSchedule
 import org.ionproject.integration.model.external.generic.CoreTerm
@@ -13,6 +8,7 @@ import org.ionproject.integration.model.external.timetable.CourseTeacher
 import org.ionproject.integration.model.external.timetable.Timetable
 import org.ionproject.integration.model.internal.core.CoreResult
 import org.ionproject.integration.service.interfaces.ICoreService
+import org.ionproject.integration.utils.Directories
 import org.ionproject.integration.utils.HttpUtils
 import org.ionproject.integration.utils.JsonUtils
 import org.ionproject.integration.utils.Try
@@ -25,60 +21,28 @@ class CoreService(private val httpUtils: HttpUtils, private val appProperties: A
     override fun pushTimetable(timetable: Timetable): Try<CoreResult> {
         var timetableJson = JsonUtils.toJson(timetable)
 
-        // return sendToCore(timetableJson, URI.create("${appProperties.coreBaseUrl}/v0/insertClassSectionEvents"))
-        File("/home/spring/timetable.json").writeText(timetableJson.orThrow())
+        File("${Directories.LOCAL_OUTPUT_DIR}/timetable.json").writeText(timetableJson.orThrow())
         return Try.of { CoreResult.SUCCESS }
     }
 
     override fun pushCourseTeacher(courseTeacher: CourseTeacher): Try<CoreResult> {
         var courseTeacherJson = JsonUtils.toJson(courseTeacher)
 
-        // return sendToCore(courseTeacherJson, URI.create("${appProperties.coreBaseUrl}/v0/insertClassSectionFaculty"))
-        File("/home/spring/courseteacher.json").writeText(courseTeacherJson.orThrow())
+        File("${Directories.LOCAL_OUTPUT_DIR}/courseteacher.json").writeText(courseTeacherJson.orThrow())
         return Try.of { CoreResult.SUCCESS }
     }
 
     override fun pushCoreTerm(coreTerm: CoreTerm): Try<CoreResult> {
         var academicCalendarJson = JsonUtils.toJson(coreTerm)
 
-        return sendToCore(academicCalendarJson, URI.create("${appProperties.coreBaseUrl}/v0/insertCalendarTerm"))
+        File("${Directories.LOCAL_OUTPUT_DIR}/academiccalendar.json").writeText(academicCalendarJson.orThrow())
+        return Try.of { CoreResult.SUCCESS }
     }
 
     override fun pushExamSchedule(coreExamSchedule: CoreExamSchedule): Try<CoreResult> {
         var coreExamScheduleJson = JsonUtils.toJson(coreExamSchedule)
 
-        return sendToCore(coreExamScheduleJson, URI.create("${appProperties.coreBaseUrl}/v0/insertClassSectionEvents"))
-    }
-
-    private fun sendToCore(json: Try<String>, url: URI): Try<CoreResult> {
-        return json
-            .map { jsonString -> put(jsonString, url) }
-            .map { httpResponse ->
-                validateStatusCode(httpResponse.statusCode())
-            }
-    }
-
-    private fun put(jsonString: String, url: URI): HttpResponse<String> {
-        var requestBuilder = HttpRequest.newBuilder()
-            .uri(url)
-            .PUT(HttpRequest.BodyPublishers.ofString(jsonString))
-            .header("Content-Type", "application/json")
-            .header("Authorization", "Bearer ${appProperties.coreToken}")
-
-        return httpUtils
-            .httpClientBuilder
-            .connectTimeout(Duration.ofSeconds(appProperties.coreRequestTimeoutSeconds.toLong()))
-            .build()
-            .send(requestBuilder.build(), HttpResponse.BodyHandlers.ofString())
-    }
-
-    private fun validateStatusCode(statusCode: Int): CoreResult {
-        return when (statusCode) {
-            in HttpURLConnection.HTTP_OK..HttpURLConnection.HTTP_NO_CONTENT -> CoreResult.SUCCESS
-            HttpURLConnection.HTTP_NOT_FOUND, HttpURLConnection.HTTP_INTERNAL_ERROR -> CoreResult.TRY_AGAIN
-            HttpURLConnection.HTTP_UNAUTHORIZED, HttpURLConnection.HTTP_FORBIDDEN -> CoreResult.EXPIRED_TOKEN
-            HttpURLConnection.HTTP_BAD_REQUEST -> CoreResult.INVALID_JSON
-            else -> CoreResult.UNRECOVERABLE_ERROR
-        }
+        File("${Directories.LOCAL_OUTPUT_DIR}/coreExamSchedule.json").writeText(coreExamScheduleJson.orThrow())
+        return Try.of { CoreResult.SUCCESS }
     }
 }
---
+++ src/main/kotlin/org/ionproject/integration/utils/StringLiterals.kt
@@ -0,0 +1,5 @@
+package org.ionproject.integration.utils
+
+enum class Directories(val path: String) {
+    LOCAL_OUTPUT_DIR("/home/test");
+}
