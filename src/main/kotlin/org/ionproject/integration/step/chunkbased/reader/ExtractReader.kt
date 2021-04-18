0a2b3698d6d10f85d342f6df8ff7c1974b237406
Grimord <grimord@hotmail.com>
2021-04-18 20:56:56 +0100

    support this year's LEIC timetable
    WIP

--- src/main/kotlin/org/ionproject/integration/step/chunkbased/reader/ExtractReader.kt
+++ src/main/kotlin/org/ionproject/integration/step/chunkbased/reader/ExtractReader.kt
@@ -2,9 +2,10 @@ package org.ionproject.integration.step.chunkbased.reader
 
 import java.nio.file.Path
 import org.ionproject.integration.extractor.implementations.ITextPdfExtractor
+import org.ionproject.integration.extractor.implementations.InstructorExtractor
 import org.ionproject.integration.extractor.implementations.TimetableExtractor
 import org.ionproject.integration.file.implementations.FileDigestImpl
-import org.ionproject.integration.model.internal.timetable.isel.RawTimetableData
+import org.ionproject.integration.model.internal.timetable.isel.RawData
 import org.ionproject.integration.utils.Try.Companion.map
 import org.ionproject.integration.utils.orThrow
 import org.springframework.batch.core.StepExecution
@@ -15,7 +16,7 @@ import org.springframework.stereotype.Component
 
 @Component("ExtractReader")
 @StepScope
-class ExtractReader : ItemReader<RawTimetableData> {
+class ExtractReader : ItemReader<RawData> {
 
     private lateinit var stepExecution: StepExecution
 
@@ -26,20 +27,25 @@ class ExtractReader : ItemReader<RawTimetableData> {
 
     private var nItems: Int = 0
 
-    override fun read(): RawTimetableData? {
+    override fun read(): RawData? {
         if (nItems > 0)
             return null
         val path = stepExecution.jobExecution.executionContext.get("file-path") as Path
         try {
             val itext = ITextPdfExtractor()
+            val tabula = TimetableExtractor()
+            val instructorParser = InstructorExtractor()
             val fd = FileDigestImpl()
 
             val headerText = itext.extract(path.toString())
-            val tabularText = TimetableExtractor.ClassSchedule.extract(path.toString())
-            val instructors = TimetableExtractor.Instructors.extract(path.toString())
+            val tabularText = tabula.extract(path.toString())
+            val instructors = instructorParser.extract(path.toString())
+
+            // val rawData = map(headerText, tabularText, instructors) { (first, second, third) -> RawData(tab.first(), txt. tab, inst) }
+            //     .orThrow()
 
             val rawData = map(headerText, tabularText, instructors) { (text, tabular, instructors) ->
-                RawTimetableData(
+                RawData(
                     tabular.first(),
                     text,
                     instructors.first()
