9aaeadc5ef4da34330180f2e197402a86aa5ed72
Grimord <grimord@hotmail.com>
2021-04-24 20:18:18 +0100

    fixing tests improving code structure

--- src/test/kotlin/org/ionproject/integration/step/chunkbased/FormatVerifierStepTest.kt
+++ src/test/kotlin/org/ionproject/integration/step/chunkbased/FormatVerifierStepTest.kt
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.batch.core.ExitStatus
