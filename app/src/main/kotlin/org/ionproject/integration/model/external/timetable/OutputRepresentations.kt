5a0cda37729e5322f0148a7c8b79994a04fc648e
Ricardo Canto <rjcanto@gmail.com>
2021-05-01 00:23:20 +0100

    First version of TimetableDto

--- app/src/main/kotlin/org/ionproject/integration/model/external/timetable/OutputRepresentations.kt
+++ app/src/main/kotlin/org/ionproject/integration/model/external/timetable/OutputRepresentations.kt
@@ -1,47 +1,4 @@
 package org.ionproject.integration.model.external.timetable
 
-import com.fasterxml.jackson.annotation.JsonInclude
-
-data class TimetableDto(
-    val creationDateTime: String,
-    val retrievalDateTime: String,
-    val school: SchoolDto,
-    val programme: ProgrammeDto,
-    val calendarTerm: String,
-    val classes: List<ClassDto>
-)
-
-data class SchoolDto(
-    val name: String,
-    val acr: String,
-)
-
-data class ProgrammeDto(
-    val name: String,
-    val acr: String,
-)
-
-data class ClassDto(
-    val acr: String,
-    val sections: List<SectionDto>
-)
-
-data class SectionDto(
-    val section: String,
-    val events: List<EventDto>,
-    val instructors: List<InstructorDto>
-)
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class EventDto(
-    val category: String,
-    val location: List<String>? = null,
-    val beginTime: String, // Format HH:MM
-    val duration: String, // Format HH:MM
-    val weekdays: String
-)
-
-data class InstructorDto(
-    val name: String,
-    val category: String
-)
+class OutputRepresentations {
+}
