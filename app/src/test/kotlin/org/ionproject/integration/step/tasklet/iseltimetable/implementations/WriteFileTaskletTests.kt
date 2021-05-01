714fef5a18f9e1913a40c112daa70234aed43076
Ricardo Canto <rjcanto@gmail.com>
2021-05-01 01:24:46 +0100

    TimetableDto test implementation

--- app/src/test/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/WriteFileTaskletTests.kt
+++ app/src/test/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/WriteFileTaskletTests.kt
@@ -1,6 +1,5 @@
 package org.ionproject.integration.step.tasklet.iseltimetable.implementations
 
-import javax.mail.internet.MimeMessage
 import org.ionproject.integration.config.AppProperties
 import org.ionproject.integration.job.ISELTimetable
 import org.ionproject.integration.model.external.timetable.CourseTeacher
@@ -8,7 +7,11 @@ import org.ionproject.integration.model.external.timetable.School
 import org.ionproject.integration.model.external.timetable.Timetable
 import org.ionproject.integration.model.external.timetable.TimetableTeachers
 import org.ionproject.integration.step.utils.SpringBatchTestUtils
+import org.ionproject.integration.utils.JsonUtils
+import org.ionproject.integration.utils.orThrow
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -21,6 +24,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.test.context.TestPropertySource
+import java.io.File
+import javax.mail.internet.MimeMessage
 
 internal class WriteFileTaskletTestFixtures {
     companion object {
@@ -97,6 +102,20 @@ class WriteFileTaskletTests {
 
         state.timetableTeachers = WriteFileTaskletTestFixtures.timetableTeachers
         writeFileTasklet = WriteFileTasklet(state)
+    }
+
+    @Test
+    fun whenANew_thenWriteFileToDisk_andConfirmFileExists() {
+        val localFilePath = "src/test/resources/timetable.json"
+        val file = File(localFilePath)
+        try {
+            file.writeText(
+                JsonUtils.toJson(state.timetableTeachers.timetable[0]).orThrow()
+            )
+            assertTrue(file.exists())
+        } finally {
+            file.deleteOnExit()
+        }
     }
 
     private fun contextContainsKey(context: ChunkContext, key: String) = context
