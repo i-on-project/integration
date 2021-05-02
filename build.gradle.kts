e61b079addbbc867e8d5b3ccd16a01505758f783
Grimord <grimord@hotmail.com>
2021-05-02 23:28:50 +0100

    feat: Serializer
    Fix gradle structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") apply false
     kotlin("plugin.spring") apply false
     id("org.springframework.boot") apply false
     id("io.spring.dependency-management") apply false
+}
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
--- file-repository/build.gradle.kts
+++ file-repository/build.gradle.kts
@@ -7,13 +7,15 @@ plugins {
 group = "org.ionproject"
 version = "0.1"
 
-repositories {
-    mavenCentral()
-}
+tasks.bootJar { enabled = false }
 
+tasks.withType<Jar> {
+    enabled = true
+}
 dependencies {
-    implementation(kotlin("stdlib"))
+    compile(project(":app"))
     implementation(project(":app"))
+    implementation(kotlin("stdlib"))
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.3")
