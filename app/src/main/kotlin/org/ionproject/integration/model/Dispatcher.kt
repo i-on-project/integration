59eedfc01f03da6f0223fca6c98efb4c4a6ba0c5
Grimord <grimord@hotmail.com>
2021-05-02 17:52:18 +0100

    feat: Dispatcher Interface
    update file structure

--- app/src/main/kotlin/org/ionproject/integration/model/Dispatcher.kt
+++ app/src/main/kotlin/org/ionproject/integration/model/Dispatcher.kt
@@ -6,22 +6,20 @@ import org.ionproject.integration.model.external.timetable.TimetableDto
  * This file contains the interfaces that must be implemented by Dispatcher modules (i.e. FileRepository).
  * It also contains all the auxiliary data classes and enums.
  */
+
+/**
+ * Interface definitions
+ */
 interface IDispatcher<T : ParsedData> {
     fun dispatch(data: T, format: OutputFormat): DispatchResult
 }
 
 interface ITimetableDispatcher<TimetableData>
 
-enum class OutputFormat {
-    YAML,
-    JSON
-}
-
-enum class DispatchResult {
-    SUCCESS,
-    FAILURE
-}
-
+/**
+ * ParsedData will be used to "transport" the final data along with the required metadata.
+ * A new implementation should be added for each file type and matching IDispatcher interface.
+ */
 sealed class ParsedData(val data: Any)
 
 data class TimetableData(
@@ -46,6 +44,16 @@ data class CalendarTerm(
     val startYear: Int,
     val term: Term
 )
+
+enum class OutputFormat {
+    YAML,
+    JSON
+}
+
+enum class DispatchResult {
+    SUCCESS,
+    FAILURE
+}
 
 enum class Term(val number: Int) {
     FALL(1),
