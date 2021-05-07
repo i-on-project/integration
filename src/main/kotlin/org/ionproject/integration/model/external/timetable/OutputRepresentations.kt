2cc5fded708ce63dfd7ceed938d1a28d6ce411dd
Ricardo Canto <rjcanto@gmail.com>
2021-05-07 01:43:20 +0100

    From Business Obj to DTO transformation

--- src/main/kotlin/org/ionproject/integration/model/external/timetable/OutputRepresentations.kt
+++ src/main/kotlin/org/ionproject/integration/model/external/timetable/OutputRepresentations.kt
@@ -9,7 +9,72 @@ data class TimetableDto(
     val programme: ProgrammeDto,
     val calendarTerm: String,
     val classes: List<ClassDto>
-)
+) {
+    fun newTimetableDto(timetableTeachers: TimetableTeachers): TimetableDto {
+        val creationDateTime = timetableTeachers.timetable[0].creationDateTime
+        val retrievalDateTime = timetableTeachers.timetable[0].retrievalDateTime
+        val school = SchoolDto(
+            timetableTeachers.timetable[0].school.name,
+            timetableTeachers.timetable[0].school.acr
+        )
+        val programme = ProgrammeDto(
+            timetableTeachers.timetable[0].programme.name,
+            timetableTeachers.timetable[0].programme.acr
+        )
+        val calendarTerm = timetableTeachers.timetable[0].calendarTerm
+
+        val courseMap = HashMap<String, MutableList<String>>()
+
+        // to get classes/courses associated to sections as per DTO definition
+        for (timetable in timetableTeachers.timetable)
+            for (course in timetable.courses) {
+                courseMap[course.label.acr] ?: mutableListOf()
+                courseMap[course.label.acr]!!.add(timetable.calendarSection)
+
+                val tempEventList = mutableListOf<EventDto>()
+                for (event in course.events) {
+                    for (weekday in event.weekday)
+                        tempEventList.add(
+                            EventDto(
+                                event.category.name,
+                                event.location,
+                                event.beginTime,
+                                event.duration,
+                                weekday
+                            )
+                        )
+                }
+
+                val tempInstructorList = mutableListOf<InstructorDto>()
+
+                for (
+                    faculty in timetableTeachers.teachers.filter { it.calendarSection == timetable.calendarSection }
+                        .flatMap { it.courses }.filter { it.classDetail.acronym == course.label.acr }.toList()
+                ) {
+                    for (instructor in faculty.instructors)
+                        tempInstructorList.add(
+                            InstructorDto(
+                                instructor.name,
+                                faculty.classDetail.type.name
+                            )
+                        )
+                }
+
+                SectionDto(timetable.calendarSection, tempEventList, tempInstructorList)
+            }
+
+        // need to have the events and instructors
+
+        return TimetableDto(
+            creationDateTime,
+            retrievalDateTime,
+            school,
+            programme,
+            calendarTerm,
+            classes
+        )
+    }
+}
 
 data class SchoolDto(
     val name: String,
--- src/main/kotlin/org/ionproject/integration/model/external/timetable/Timetable.kt
+++ src/main/kotlin/org/ionproject/integration/model/external/timetable/Timetable.kt
@@ -1,6 +1,8 @@
 package org.ionproject.integration.model.external.timetable
 
 data class Timetable(
+    var creationDateTime: String = "",
+    var retrievalDateTime: String = "",
     var school: School = School(),
     var programme: Programme = Programme(),
     var calendarTerm: String = "",
