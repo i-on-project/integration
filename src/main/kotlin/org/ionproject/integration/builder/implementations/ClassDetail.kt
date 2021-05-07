9895b42cbcab1129a26226a7f245bcd23a007df8
Ricardo Canto <rjcanto@gmail.com>
2021-05-07 01:43:20 +0100

    From Business Obj to DTO transformation

--- src/main/kotlin/org/ionproject/integration/builder/implementations/ClassDetail.kt
+++ src/main/kotlin/org/ionproject/integration/builder/implementations/ClassDetail.kt
@@ -1,11 +1,12 @@
 package org.ionproject.integration.builder.implementations
 
+import org.ionproject.integration.model.external.timetable.EventCategory
 import java.lang.Integer.max
 
 data class ClassDetail(
     val acronym: String,
     val location: String,
-    val type: String
+    val type: EventCategory
 ) {
     companion object {
         fun from(text: String): ClassDetail {
@@ -13,7 +14,13 @@ data class ClassDetail(
 
             val acronym = text.substringBefore('[').trim()
             val location = text.substring(text.lastIndexOf(')') + 1).trim()
-            val type = text.substring(firstIndex + 1, max(text.indexOf(')', firstIndex), 0)).trim()
+            val type = when (text.substring(firstIndex + 1, max(text.indexOf(')', firstIndex), 0)).trim()) {
+                "L" -> EventCategory.LAB
+                "T" -> EventCategory.LECTURE
+                "T/P" -> EventCategory.LECTURE_PRACTICE
+                "P" -> EventCategory.PRACTICE
+                else -> EventCategory.LECTURE // TODO throw exception?
+            }
 
             return ClassDetail(acronym, location, type)
         }
