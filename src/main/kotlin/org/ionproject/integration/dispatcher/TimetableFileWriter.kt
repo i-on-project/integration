4cf0590dfdc3dbdd10c9d37d76d91d426ca5ed0e
Grimord <grimord@hotmail.com>
2021-05-09 03:48:06 +0100

    Update TimetableFileWriter.kt
    Co-Authored-By: Ricardo Canto <rjcanto@gmail.com>

--- src/main/kotlin/org/ionproject/integration/dispatcher/TimetableFileWriter.kt
+++ src/main/kotlin/org/ionproject/integration/dispatcher/TimetableFileWriter.kt
@@ -1,15 +1,22 @@
 package org.ionproject.integration.dispatcher
 
+import org.ionproject.integration.config.AppProperties
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.io.File
 
-const val STAGING_ROOT = "staging"
-const val REPOSITORY_ROOT = "integration-data"
 private const val TIMETABLE_FILENAME = "timetable"
 private const val PROGRAMMES = "programmes"
 
 @Component
 class TimetableFileWriter(private val serializer: ISerializer) {
+
+    @Autowired
+    private lateinit var props: AppProperties
+
+    val staging by lazy { props.stagingDir }
+    val repositoryName by lazy { props.gitRepository }
+
     fun write(timetable: TimetableData, format: OutputFormat): File {
 
         val dir = getDirectory(timetable)
@@ -25,15 +32,22 @@ class TimetableFileWriter(private val serializer: ISerializer) {
     }
 
     private fun getDirectory(timetable: TimetableData): Filepath {
-        val segments = listOf(
-            STAGING_ROOT,
-            REPOSITORY_ROOT,
+        val pathType = if (staging.first() == '/') Filepath.PathType.ABSOLUTE else Filepath.PathType.RELATIVE
+
+        val rootSegments = if (staging.contains('/')) {
+            staging.split('/').filter(String::isNotBlank)
+        } else {
+            listOf(staging)
+        }
+
+        val segments = rootSegments + listOf(
+            repositoryName,
             timetable.programme.institution.domain,
             PROGRAMMES,
             timetable.programme.acronym,
             timetable.term.toString()
         )
 
-        return Filepath(segments, caseType = Filepath.CaseType.LOWER)
+        return Filepath(segments, pathType, Filepath.CaseType.LOWER)
     }
 }
