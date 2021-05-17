6bcc33d2f783f02b84408f7192634fa8c340dbe8
Ricardo Canto <rjcanto@gmail.com>
2021-05-17 09:58:58 +0100

    The PDF extractor gets the creation date from PDF document and in the set of the Common Data both are added to the Business Object properties

--- src/main/kotlin/org/ionproject/integration/extractor/implementations/ITextPdfExtractor.kt
+++ src/main/kotlin/org/ionproject/integration/extractor/implementations/ITextPdfExtractor.kt
@@ -1,11 +1,15 @@
 package org.ionproject.integration.extractor.implementations
 
+import com.itextpdf.kernel.pdf.PdfDate
 import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfName
 import com.itextpdf.kernel.pdf.PdfReader
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor
 import org.ionproject.integration.extractor.exceptions.PdfExtractorException
 import org.ionproject.integration.extractor.interfaces.IPdfExtractor
 import org.ionproject.integration.utils.Try
+import java.text.SimpleDateFormat
+import java.util.TimeZone
 
 class ITextPdfExtractor : IPdfExtractor {
     /**
@@ -24,7 +28,10 @@ class ITextPdfExtractor : IPdfExtractor {
                 .map { reader -> PdfDocument(reader) }
                 .map { pdfDoc ->
                     for (i in 1..pdfDoc.numberOfPages)
-                        data.add(PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i)))
+                        data.add(
+                            PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i)) +
+                                getCreationDateFromPdfDocument(pdfDoc)
+                        )
                 }
                 .map { data.toList() }
                 .mapError { PdfExtractorException("Itext cannot process file") }
@@ -32,5 +39,13 @@ class ITextPdfExtractor : IPdfExtractor {
             pdfReader
                 .map { reader -> reader.close() }
         }
+    }
+
+    private fun getCreationDateFromPdfDocument(pdfDocument: PdfDocument): String {
+        val creationDateString = pdfDocument.documentInfo.getMoreInfo(PdfName.CreationDate.value)
+        val creationDateCalendar = PdfDate.decode(creationDateString)
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+        val dateFormat = SimpleDateFormat("yyyyMMdd'T'HHmmssX")
+        return dateFormat.format(creationDateCalendar.time)
     }
 }
