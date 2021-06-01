b29773b27e12e9781e08eea337ec754b86b3aa80
Ricardo Canto <rjcanto@gmail.com>
2021-06-02 00:27:08 +0100

    Academic Calendar Job Creation and DTOs

--- src/main/kotlin/org/ionproject/integration/extractor/implementations/AcademicCalendarExtractor.kt
+++ src/main/kotlin/org/ionproject/integration/extractor/implementations/AcademicCalendarExtractor.kt
@@ -7,21 +7,21 @@ import org.ionproject.integration.extractor.interfaces.IPdfExtractor
 import org.ionproject.integration.utils.Try
 import technology.tabula.CommandLineApp
 
-object TimetableExtractor {
+object AcademicCalendarExtractor {
     // Arguments to pass to tabula
     // -g = Guess the portion of the page to analyze per page
     // -t = Force PDF to be extracted using stream-mode
     // -p = Page range
     // -f = Output format JSON
     // -a top, left, bottom, right
-    val ClassSchedule = object : IPdfExtractor {
+    val winterSemester = object : IPdfExtractor {
         override fun extract(pdfPath: String): Try<List<String>> =
-            processPdf(pdfPath, "-a 0,0,670,558", "-g", "-l", "-p", "all", "-f", "JSON")
+            processPdf(pdfPath, "-a 300,55,490,540", "-g", "-l", "-p", "all", "-f", "JSON")
     }
 
-    val Instructors = object : IPdfExtractor {
+    val summerSemester = object : IPdfExtractor {
         override fun extract(pdfPath: String): Try<List<String>> =
-            processPdf(pdfPath, "-a 671,0,828,558", "-g", "-t", "-p", "all", "-f", "JSON")
+            processPdf(pdfPath, "-a 596,55,750,540", "-g", "-t", "-p", "all", "-f", "JSON")
     }
 
     private fun processPdf(path: String, vararg tabulaArguments: String): Try<List<String>> {
