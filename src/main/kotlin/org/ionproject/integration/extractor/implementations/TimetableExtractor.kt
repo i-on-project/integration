fe70c866c58c8dd747523c3aa0c7f36bf0eeddb3
Ricardo Canto <rjcanto@gmail.com>
2021-06-05 22:48:16 +0100

    business object creation

--- src/main/kotlin/org/ionproject/integration/extractor/implementations/TimetableExtractor.kt
+++ src/main/kotlin/org/ionproject/integration/extractor/implementations/TimetableExtractor.kt
@@ -1,11 +1,8 @@
 package org.ionproject.integration.extractor.implementations
 
-import java.io.File
-import org.apache.commons.cli.DefaultParser
-import org.ionproject.integration.extractor.exceptions.PdfExtractorException
 import org.ionproject.integration.extractor.interfaces.IPdfExtractor
+import org.ionproject.integration.utils.PdfUtils
 import org.ionproject.integration.utils.Try
-import technology.tabula.CommandLineApp
 
 object TimetableExtractor {
     // Arguments to pass to tabula
@@ -16,26 +13,11 @@ object TimetableExtractor {
     // -a top, left, bottom, right
     val ClassSchedule = object : IPdfExtractor {
         override fun extract(pdfPath: String): Try<List<String>> =
-            processPdf(pdfPath, "-a 0,0,670,558", "-g", "-l", "-p", "all", "-f", "JSON")
+            PdfUtils.processPdf(pdfPath, "-a 0,0,670,558", "-g", "-l", "-p", "all", "-f", "JSON")
     }
 
     val Instructors = object : IPdfExtractor {
         override fun extract(pdfPath: String): Try<List<String>> =
-            processPdf(pdfPath, "-a 671,0,828,558", "-g", "-t", "-p", "all", "-f", "JSON")
-    }
-
-    private fun processPdf(path: String, vararg tabulaArguments: String): Try<List<String>> {
-        if (path.isEmpty()) return Try.ofError<PdfExtractorException>(PdfExtractorException("Empty path"))
-        val pdfFile = File(path)
-        if (!pdfFile.exists()) return Try.ofError<PdfExtractorException>(PdfExtractorException("File doesn't exist"))
-
-        val parser = DefaultParser()
-        val data = StringBuilder()
-
-        return Try.of { arrayOf(pdfFile.absolutePath, *tabulaArguments) }
-            .map { args -> parser.parse(CommandLineApp.buildOptions(), args) }
-            .map { cmd -> CommandLineApp(data, cmd).extractTables(cmd) }
-            .map { listOf(data.toString()) }
-            .mapError { PdfExtractorException("Tabula cannot process file") }
+            PdfUtils.processPdf(pdfPath, "-a 671,0,828,558", "-g", "-t", "-p", "all", "-f", "JSON")
     }
 }
--- src/main/kotlin/org/ionproject/integration/model/internal/calendar/isel/RawCalendarData.kt
+++ src/main/kotlin/org/ionproject/integration/model/internal/calendar/isel/RawCalendarData.kt
@@ -1,9 +1,7 @@
 package org.ionproject.integration.model.internal.calendar.isel
 
 data class RawCalendarData(
-    val calendarData: String,
     val textData: List<String>,
-    val summerSemester: String,
-    val winterSemester: String,
+    val table: String,
     val creationDate: String
 )
