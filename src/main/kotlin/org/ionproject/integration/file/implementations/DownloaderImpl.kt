600ca85bd922895a70085dda4207acb4b70c6200
Grimord <grimord@hotmail.com>
2021-06-09 22:58:30 +0100

    initial implementation

---
+++ src/main/kotlin/org/ionproject/integration/file/implementations/DownloaderImpl.kt
@@ -0,0 +1,21 @@
+package org.ionproject.integration.file.implementations
+
+import org.ionproject.integration.file.interfaces.IFileDownloader
+import org.ionproject.integration.utils.Try
+import org.springframework.stereotype.Service
+import java.net.URI
+import java.nio.file.Path
+
+@Service
+class DownloaderImpl(val channelProvider: ChannelProvider) : IFileDownloader {
+    override fun download(uri: URI, localDestination: Path): Try<Path> {
+        channelProvider.getRemoteChannel(uri).use { remoteChannel ->
+            channelProvider.getLocalChannel(localDestination).use { fileChannel ->
+                return Try.of {
+                    fileChannel.transferFrom(remoteChannel, 0, Long.MAX_VALUE)
+                    localDestination
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/org/ionproject/integration/file/interfaces/IBytesFormatChecker.kt
+++ src/main/kotlin/org/ionproject/integration/file/interfaces/IBytesFormatChecker.kt
@@ -1,7 +1,5 @@
 package org.ionproject.integration.file.interfaces
 
-import org.ionproject.integration.model.internal.generic.JobType
-
 interface IBytesFormatChecker {
-    fun checkFormat(bytes: ByteArray, jobType: JobType?)
+    fun isValidFormat(bytes: ByteArray): Boolean
 }
