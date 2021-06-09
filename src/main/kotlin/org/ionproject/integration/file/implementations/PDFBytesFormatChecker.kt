aabce47b09b279c2400eca8661700bb7c26e3089
Grimord <grimord@hotmail.com>
2021-06-09 23:34:59 +0100

    improve readability of PDFBytesFormatChecker.kt

--- src/main/kotlin/org/ionproject/integration/file/implementations/PDFBytesFormatChecker.kt
+++ src/main/kotlin/org/ionproject/integration/file/implementations/PDFBytesFormatChecker.kt
@@ -15,10 +15,17 @@ class PDFBytesFormatChecker : IBytesFormatChecker {
     override fun isValidFormat(bytes: ByteArray): Boolean {
         if (bytes.size < HEADER_LENGTH)
             return false
-        val minorVersion: Char = bytes[HEADER_MIN_VERSION_POSITION].toInt().toChar()
-        val headerBytes: ByteArray = bytes.slice(HEADER_RANGE).toByteArray()
-        val header = String(headerBytes, Charsets.UTF_8)
+        val minorVersion = getMinorVersion(bytes)
+        val header = getFileHeader(bytes)
 
         return header == PDF_HEADER && minorVersion in MINIMUM_PDF_VERSION..MAXIMUM_PDF_VERSION
+    }
+
+    private fun getMinorVersion(bytes: ByteArray): Char =
+        bytes[HEADER_MIN_VERSION_POSITION].toInt().toChar()
+
+    private fun getFileHeader(bytes: ByteArray): String {
+        val headerBytes: ByteArray = bytes.slice(HEADER_RANGE).toByteArray()
+        return String(headerBytes, Charsets.UTF_8)
     }
 }
