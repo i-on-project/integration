31008e8b7772b3f83c015bcc6f4b74d07d24adc9
Grimord <grimord@hotmail.com>
2021-06-09 23:30:38 +0100

    remove useless email notificaton from post processing step

--- src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/PostUploadTasklet.kt
+++ src/main/kotlin/org/ionproject/integration/step/tasklet/iseltimetable/implementations/PostUploadTasklet.kt
@@ -1,11 +1,7 @@
 package org.ionproject.integration.step.tasklet.iseltimetable.implementations
 
 import javax.sql.DataSource
-import org.ionproject.integration.alert.implementations.EmailAlertChannel
-import org.ionproject.integration.alert.implementations.EmailAlertService
 import org.ionproject.integration.hash.implementations.HashRepositoryImpl
-import org.ionproject.integration.utils.EmailUtils
-import org.ionproject.integration.utils.JobResult
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.StepScope
@@ -14,10 +10,11 @@ import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.stereotype.Component
 
-@Component("PostUploadTasklet")
+const val POST_UPLOAD_TASKLET_NAME = "PostUploadTasklet"
+
+@Component(POST_UPLOAD_TASKLET_NAME)
 @StepScope
 class PostUploadTasklet() : Tasklet {
     private val log = LoggerFactory.getLogger(PostUploadTasklet::class.java)
@@ -25,46 +22,18 @@ class PostUploadTasklet() : Tasklet {
     @Autowired
     private lateinit var ds: DataSource
 
-    @Autowired
-    private lateinit var sender: JavaMailSenderImpl
-
     @Value("#{jobParameters['jobId']}")
     private lateinit var jobId: String
-
-    @Value("#{jobParameters['alertRecipient']}")
-    private lateinit var alertRecipient: String
-
-    @Value("#{jobParameters['srcRemoteLocation']}")
-    private lateinit var srcRemoteLocation: String
 
     override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
         val hr = HashRepositoryImpl(ds)
 
-        val fileHash =
-            chunkContext.stepContext.stepExecution.jobExecution.executionContext.get("file-hash") as ByteArray
+        val fileHash = getHash(chunkContext)
         hr.putHash(jobId, fileHash)
-
-        sendEmail(chunkContext.stepContext.jobName)
 
         return RepeatStatus.FINISHED
     }
 
-    private fun sendEmail(jobName: String) {
-
-        val asset = srcRemoteLocation
-            .substring(srcRemoteLocation.lastIndexOf('/') + 1, srcRemoteLocation.length)
-
-        val conf = EmailUtils.configure(
-            jobName,
-            JobResult.COMPLETED_SUCCESSFULLY,
-            alertRecipient,
-            asset,
-            null
-        )
-        val channel = EmailAlertChannel(conf, sender)
-        val alertService = EmailAlertService(channel)
-        alertService.sendEmail()
-
-        log.info("Email sent successfully")
-    }
+    private fun getHash(chunkContext: ChunkContext): ByteArray =
+        chunkContext.stepContext.stepExecution.jobExecution.executionContext.get("file-hash") as ByteArray
 }
