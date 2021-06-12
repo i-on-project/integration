23ce766a51d5992d27d9f82e660db560eae9ffa7
Grimord <grimord@hotmail.com>
2021-06-12 13:26:14 +0100

    improve dtos

--- src/main/kotlin/org/ionproject/integration/ui/dto/InputDto.kt
+++ src/main/kotlin/org/ionproject/integration/ui/dto/InputDto.kt
@@ -13,39 +13,73 @@ class InputDto(
     val programmeRepo: IProgrammeRepository
 ) {
     fun getTimetableJobRequest(dto: CreateTimetableJobDto): JobEngine.TimetableJobRequest {
-        runCatching {
-            requireNotNull(dto.format) { "format" }
-            requireNotNull(dto.institution) { "institution" }
-            requireNotNull(dto.programme) { "programme" }
-        }.onFailure { throw ArgumentException("Parameter '${it.message}' missing") }
+        val safeDto = dto.toSafeDto()
 
-        val format = OutputFormat.of(dto.format!!)
-        val institution = institutionRepo.getInstitutionByIdentifier(dto.institution!!)
-        val programme = programmeRepo.getProgrammeByAcronymAndInstitution(dto.programme!!, institution)
+        val format = OutputFormat.of(safeDto.format)
+        val institution = institutionRepo.getInstitutionByIdentifier(safeDto.institution)
+        val programme = programmeRepo.getProgrammeByAcronymAndInstitution(safeDto.programme, institution)
 
         return JobEngine.TimetableJobRequest(format, institution, programme)
     }
 
     fun getCalendarJobRequest(dto: CreateCalendarJobDto): JobEngine.CalendarJobRequest {
-        runCatching {
-            requireNotNull(dto.format) { "format" }
-            requireNotNull(dto.institution) { "institution" }
-        }.onFailure { throw ArgumentException("Parameter '${it.message}' missing") }
+        val safeDto = dto.toSafeDto()
 
-        val format = OutputFormat.of(dto.format!!)
-        val institution = institutionRepo.getInstitutionByIdentifier(dto.institution!!)
+        val format = OutputFormat.of(safeDto.format)
+        val institution = institutionRepo.getInstitutionByIdentifier(safeDto.institution)
 
         return JobEngine.CalendarJobRequest(format, institution)
     }
 }
 
+sealed interface JobDto {
+    val institution: String?
+    val format: String?
+    fun toSafeDto(): SafeJobDto
+}
+
+sealed interface SafeJobDto {
+    val institution: String
+    val format: String
+}
+
 data class CreateTimetableJobDto(
-    val institution: String? = null,
+    override val institution: String? = null,
     val programme: String? = null,
-    val format: String? = null
-)
+    override val format: String? = null
+) : JobDto {
+    override fun toSafeDto(): SafeTimetableJobDto {
+        validateDto(this)
+        return SafeTimetableJobDto(institution!!, programme!!, format!!)
+    }
+}
 
 data class CreateCalendarJobDto(
-    val institution: String? = null,
-    val format: String? = null
-)
+    override val institution: String? = null,
+    override val format: String? = null
+) : JobDto {
+    override fun toSafeDto(): SafeCalendarJobDto {
+        validateDto(this)
+        return SafeCalendarJobDto(institution!!, format!!)
+    }
+}
+
+data class SafeTimetableJobDto(
+    override val institution: String,
+    val programme: String,
+    override val format: String,
+) : SafeJobDto
+
+data class SafeCalendarJobDto(
+    override val institution: String,
+    override val format: String,
+) : SafeJobDto
+
+private fun validateDto(dto: JobDto) {
+    runCatching {
+        requireNotNull(dto.format) { "format" }
+        requireNotNull(dto.institution) { "institution" }
+        if (dto is CreateTimetableJobDto)
+            requireNotNull(dto.programme) { "programme" }
+    }.onFailure { throw ArgumentException("Parameter '${it.message}' missing") }
+}
