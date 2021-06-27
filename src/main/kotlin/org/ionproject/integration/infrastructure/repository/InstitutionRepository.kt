164d52cac7abec2cbbe3e05e5af0d168fbe101d3
Grimord <grimord@hotmail.com>
2021-06-27 19:50:52 +0100

    add InstitutionRepository based on config file

--- src/main/kotlin/org/ionproject/integration/infrastructure/repository/InstitutionRepository.kt
+++ src/main/kotlin/org/ionproject/integration/infrastructure/repository/InstitutionRepository.kt
@@ -1,19 +1,59 @@
 package org.ionproject.integration.infrastructure.repository
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import org.ionproject.integration.application.config.AppProperties
 import org.ionproject.integration.domain.common.InstitutionModel
-import org.ionproject.integration.domain.common.SupportedInstitutions
-import org.ionproject.integration.infrastructure.exception.ArgumentException
+import org.ionproject.integration.domain.common.Language
+import org.ionproject.integration.infrastructure.text.IgnoredWords
+import org.ionproject.integration.infrastructure.text.generateAcronym
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import java.net.URI
 
 interface IInstitutionRepository {
     fun getInstitutionByIdentifier(identifier: String): InstitutionModel
 }
 
 @Service
-class InstitutionRepositoryImpl : IInstitutionRepository {
-    override fun getInstitutionByIdentifier(identifier: String): InstitutionModel =
-        SupportedInstitutions.values()
-            .map(SupportedInstitutions::institution)
-            .firstOrNull { it.identifier == identifier }
-            ?: throw ArgumentException("Institution with identifier $identifier not found")
+class InstitutionRepositoryFile : IInstitutionRepository {
+    private val mapper by lazy { ObjectMapper(YAMLFactory()) }
+
+    @Autowired
+    private lateinit var props: AppProperties
+
+    override fun getInstitutionByIdentifier(identifier: String): InstitutionModel {
+        val type = mapper.typeFactory.constructCollectionType(List::class.java, InstitutionDto::class.java)
+        val file = props.configurationFile.asFile
+        val supportedInstitutions: List<InstitutionDto> = mapper.readValue(file, type)
+
+        return supportedInstitutions.first().toModel()
+    }
 }
+
+private data class InstitutionDto(
+    val name: String = "",
+    val identifier: String = "",
+    val resources: List<ResourceDto> = listOf(),
+    val programmes: List<ProgrammeDto> = listOf()
+) {
+    fun toModel(): InstitutionModel {
+        return InstitutionModel(
+            name = this.name,
+            acronym = generateAcronym(this.name, IgnoredWords.of(Language.PT)),
+            identifier = this.identifier,
+            academicCalendarUri = URI(this.resources.first().uri)
+        )
+    }
+}
+
+private data class ResourceDto(
+    val type: String = "",
+    val uri: String = ""
+)
+
+private data class ProgrammeDto(
+    val name: String = "",
+    val acronym: String? = null,
+    val resources: List<ResourceDto> = listOf()
+)
