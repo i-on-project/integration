dc233213e9d1639469b88e66c9e54f5b7b435b9b
Grimord <grimord@hotmail.com>
2021-07-03 20:40:55 +0100

    add OutputFormatTests add support for alternative format name for YAML (as yml) in OutputFormat.kt

--- src/main/kotlin/org/ionproject/integration/infrastructure/file/OutputFormat.kt
+++ src/main/kotlin/org/ionproject/integration/infrastructure/file/OutputFormat.kt
@@ -1,16 +1,23 @@
 package org.ionproject.integration.infrastructure.file
 
 import org.ionproject.integration.infrastructure.exception.ArgumentException
+import org.ionproject.integration.infrastructure.text.containsCaseInsensitive
 
 internal const val INVALID_FORMAT_ERROR = "Invalid format: %s"
 
-enum class OutputFormat(val extension: String) {
-    YAML(".yml"),
+enum class OutputFormat(val extension: String, private val alternativeNames: List<String> = emptyList()) {
+    YAML(".yml", listOf("yml")),
     JSON(".json");
 
     companion object {
-        fun of(name: String): OutputFormat =
-            values().firstOrNull { it.name.equals(name.trim(), ignoreCase = true) }
-                ?: throw ArgumentException(INVALID_FORMAT_ERROR.format(name.trim()))
+        fun of(name: String): OutputFormat {
+            val trimName = name.trim()
+            return values().firstOrNull { format ->
+                format.nameEquals(trimName) || format.alternativeNames.containsCaseInsensitive(trimName)
+            }
+                ?: throw ArgumentException(INVALID_FORMAT_ERROR.format(trimName))
+        }
     }
+
+    private fun nameEquals(name: String): Boolean = this.name.equals(name, ignoreCase = true)
 }
