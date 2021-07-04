7a164e1c44af2cbf5e3f0bec3af44138f76d428c
Grimord <grimord@hotmail.com>
2021-07-04 19:43:19 +0100

    add JobNotFoundException definition make JobNotFoundException errors to return new Problem type

--- src/main/kotlin/org/ionproject/integration/ui/dto/Problem.kt
+++ src/main/kotlin/org/ionproject/integration/ui/dto/Problem.kt
@@ -2,11 +2,12 @@ package org.ionproject.integration.ui.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.ionproject.integration.infrastructure.exception.ArgumentException
+import org.ionproject.integration.infrastructure.exception.JobNotFoundException
 import org.springframework.http.HttpStatus
 import javax.servlet.http.HttpServletRequest
 
-private const val ARGUMENT_EX_URI =
-    "https://github.com/i-on-project/integration/blob/master/docs/infrastructure/ArgumentException.md"
+private const val ARGUMENT_EX_URI = "https://github.com/i-on-project/integration/blob/master/docs/infrastructure/ArgumentException.md"
+private const val JOB_NOT_FOUND_EX_URI = "https://github.com/i-on-project/integration/blob/master/docs/infrastructure/JobNotFoundException.md"
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Problem(
@@ -17,12 +18,21 @@ data class Problem(
     val instance: String? = null
 ) {
     companion object Factory {
-        fun of(argumentException: ArgumentException, request: HttpServletRequest): Problem {
-            return Problem(
+        fun of(argumentException: ArgumentException, request: HttpServletRequest): Problem =
+            Problem(
                 title = HttpStatus.BAD_REQUEST.reasonPhrase,
                 status = HttpStatus.BAD_REQUEST.value(),
                 detail = argumentException.message,
                 type = ARGUMENT_EX_URI,
+                instance = request.requestURI
+            )
+
+        fun of(jobNotFoundException: JobNotFoundException, request: HttpServletRequest): Problem {
+            return Problem(
+                title = HttpStatus.NOT_FOUND.reasonPhrase,
+                status = HttpStatus.NOT_FOUND.value(),
+                detail = jobNotFoundException.message,
+                type = JOB_NOT_FOUND_EX_URI,
                 instance = request.requestURI
             )
         }
