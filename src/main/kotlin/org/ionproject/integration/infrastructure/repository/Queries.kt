d3381dac7b16fec1eb8ba144a1afa0602b0100b6
Grimord <grimord@hotmail.com>
2021-07-05 02:00:32 +0100

    add enddate field to job data

--- src/main/kotlin/org/ionproject/integration/infrastructure/repository/Queries.kt
+++ src/main/kotlin/org/ionproject/integration/infrastructure/repository/Queries.kt
@@ -1,11 +1,30 @@
 package org.ionproject.integration.infrastructure.repository
 
+/**
+ * Ordering on this enum MUST match ordering on CREATE_JOBS_VIEW_QUERY
+ */
+internal enum class JobQueryFields {
+    ID,
+    NAME,
+    CREATION_DATE,
+    START_DATE,
+    END_DATE,
+    STATUS,
+    FORMAT,
+    INSTITUTION,
+    PROGRAMME,
+    URI;
+
+    val index = ordinal + 1
+}
+
 const val CREATE_JOBS_VIEW_QUERY = """create or replace view vw_job_detail
 as
 SELECT bje.job_instance_id as id
      , bji.job_name        as name
      , bje.create_time at time zone 'utc'     as creation_date
      , bje.start_time at time zone 'utc'     as start_date
+     , bje.end_time at time zone 'utc'     as end_time
      ,CASE 
 		WHEN bje.STATUS IN ('STARTED','STARTING')
 			AND (bje.create_time at time zone 'utc') < ((CURRENT_TIMESTAMP at time zone 'utc') - interval '1 hours')
