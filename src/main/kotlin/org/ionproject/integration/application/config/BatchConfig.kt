4558c54c0631525407283802a3b31c192265815f
Grimord <grimord@hotmail.com>
2021-07-20 00:11:06 +0100

    change default spring batch isolation level to avoid serialization errors using postgres under heavy load

--- src/main/kotlin/org/ionproject/integration/application/config/BatchConfig.kt
+++ src/main/kotlin/org/ionproject/integration/application/config/BatchConfig.kt
@@ -1,12 +1,15 @@
 package org.ionproject.integration.application.config
 
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.batch.core.launch.support.SimpleJobLauncher
 import org.springframework.batch.core.repository.JobRepository
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import javax.sql.DataSource
 
 const val LAUNCHER_NAME = "asyncLauncher"
 
@@ -30,5 +33,16 @@ class BatchConfig {
         setTaskExecutor(executor)
 
         afterPropertiesSet()
+    }
+}
+
+@Configuration
+class CustomBatchConfigurer(val dataSource: DataSource) : DefaultBatchConfigurer() {
+    override fun createJobRepository(): JobRepository {
+        val factory = JobRepositoryFactoryBean()
+        factory.setDataSource(dataSource)
+        factory.transactionManager = transactionManager
+        factory.setIsolationLevelForCreate("ISOLATION_REPEATABLE_READ")
+        return factory.getObject()
     }
 }
